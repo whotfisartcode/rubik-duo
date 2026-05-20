9c56474340faf55c7abbf98815989abc444230ae
Artem Kozin <kozinartem.1910@gmail.com>
2026-05-20 12:30:56 +0300

    Tune home hero typography

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "rubik-duo-cache-v21";
+const CACHE_NAME = "rubik-duo-cache-v22";
 const APP_ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=21",
-  "./app.js?v=21",
+  "./styles.css?v=22",
+  "./app.js?v=22",
   "./manifest.json",
   "./icon.svg"
 ];
